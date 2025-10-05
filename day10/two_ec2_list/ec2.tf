7cd1ccedb5e9d715b72b51029814533619deedd0
pardeep1916P <chaitanyacharan07@gmail.com>
2025-10-05 10:11:22 +0530

    tf_day_10

--- day10/two_ec2_list/ec2.tf
+++ day10/two_ec2_list/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "my_ec2" {
   instance_type = var.ec2_list_config[count.index].instance_type
   subnet_id     = element(aws_subnet.subnet[*].id, count.index % length(aws_subnet.subnet))
   tags = {
-    Name =  "tf-ec2-0${count.index + 1}"
+    Name = "tf-ec2-0${count.index + 1}"
   }
 }
 
